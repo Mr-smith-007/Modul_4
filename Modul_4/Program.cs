084e25d63c333019cd30774fff4facdbc5337027
Repnikov.A <repnikov.a@florcom.ru>
2023-07-03 16:53:12 +0300

    Задание 4.3.7

--- Modul_4/Program.cs
+++ Modul_4/Program.cs
@@ -4,39 +4,15 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
-
-        var color = Console.ReadLine();
-
-        switch (color)
+        Console.WriteLine("Введите имя");
+        var name = Console.ReadLine();
+        Console.WriteLine("Ваше имя наоборот:");
+        char [] eman = new char[name.Length];
+        for (int i = 0; i < eman.Length; i++)
         {
-            case "red":
-                Console.BackgroundColor = ConsoleColor.Red;
-                Console.ForegroundColor = ConsoleColor.Black;
-
-                Console.WriteLine("Your color is red!");
-                break;
-
-            case "green":
-                Console.BackgroundColor = ConsoleColor.Green;
-                Console.ForegroundColor = ConsoleColor.Black;
-
-                Console.WriteLine("Your color is green!");
-                break;
-
-            case "cyan":
-                Console.BackgroundColor = ConsoleColor.Cyan;
-                Console.ForegroundColor = ConsoleColor.Black;
-
-                Console.WriteLine("Your color is cyan");
-                break;
-
-            default:
-                Console.BackgroundColor = ConsoleColor.Yellow;
-                Console.ForegroundColor = ConsoleColor.Red;
-
-                Console.WriteLine("Your color is yellow!");
-                break;
+            eman[i] = name[name.Length -i -1];
+            Console.Write(eman[i] + " ");
         }
+
     }
 }

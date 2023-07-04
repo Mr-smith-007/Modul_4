bf72ae501a57488a0a9df978cf86919f6f1b3ad7
Repnikov.A <repnikov.a@florcom.ru>
2023-07-04 13:55:44 +0300

    Задание 4.3.17

--- Modul_4/Program.cs
+++ Modul_4/Program.cs
@@ -1,18 +1,34 @@
-﻿using System;
-
-class MainClass
+﻿class MainClass
 {
     public static void Main(string[] args)
     {
-        int [,] array = { {-5, 6, 9, 1, 2, -3},{ -8, 8, 1, 1, 2, -3 } };
-        int kp = 0;
-        foreach (int x in array)
+        int[,] array = { {-5, 6, 9, 1, 2, -3}, {-8, 8, 1, 1, 2, -3} };
+        int t;
+        for (int i = 0; i < array.GetUpperBound(0) +1; i++)
         {
-            if (x > 0)
-                kp++;
+            for (int j = 0; j < array.GetUpperBound(1) +1; j++)
+            {
+                for (int k = j + 1; k < array.GetUpperBound(1) + 1; k++)
+                {
+                    if (array[i, j] > array[i, k])
+                    {
+                        t = array[i, k];
+                        array[i, k] = array[i, j];
+                        array[i, j] = t;
+                    }
+                }
+            }
         }
-        Console.WriteLine($"Количество положительных элементов массива {kp}");
-        
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+                Console.Write(array[i, j] + " ");
+            Console.WriteLine();
+        }
+        Console.ReadKey();
+
+
+
     }
 }
 
